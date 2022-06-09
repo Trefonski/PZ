676898e99993a735c973f9c6b30aeb915b7f87c5
pawszmir <pawszmir@gmail.com>
2022-06-09 14:50:36 +0200

    Items navigation Init

---
+++ ClientApp/src/app/items-view/items-nav/items-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-items-nav',
+  templateUrl: './items-nav.component.html',
+  styleUrls: ['./items-nav.component.css']
+
+})
+export class ItemsNavComponent {
+
+}
