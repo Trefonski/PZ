ab4c36262f87b68f7c83ce5412fe333b88e0605c
pawszmir <pawszmir@gmail.com>
2022-06-09 14:01:38 +0200

    Home page style change

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Items } from '../items';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  items=Items;
 }
