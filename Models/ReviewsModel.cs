e36b4c635ab9e6f5fd69a62d122282edc9f43e4c
Michal Trefonski <trefonski@gmail.com>
2022-06-10 20:14:57 +0200

    Fix Reviews enum

--- Models/ReviewsModel.cs
+++ Models/ReviewsModel.cs
@@ -2,11 +2,13 @@ namespace PZ.Models
 {
     public class Reviews
     {
+        public enum RatingType {oneStar=1, twoStars=2, threeStars=3, fourStars=4, fiveStars=5}
+
         public uint ID_Client {get; set;}
         public string ID_Item {get; set;}
-        public enum Rating {oneStar=1, twoStars=2, threeStars=3, fourStars=4, fiveStars=5}
         public string Title {get; set;}
         public string Body {get; set;}
+        public RatingType Rating { get; set; }
         public DateTime DatePosted {get; set;}
 
         //FK
