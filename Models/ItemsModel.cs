656fe19431564bfc1e4ea71e31f31d8671fc2c44
Michal Trefonski <trefonski@gmail.com>
2022-06-10 20:14:37 +0200

    Fix Items Enum

--- Models/ItemsModel.cs
+++ Models/ItemsModel.cs
@@ -2,6 +2,8 @@ namespace PZ.Models
 {
     public class Items
     {
+        public enum SexType { none=0,mens=1, womens=2, unisex=3, boy=4, girl=5, unisexChild=6 }
+
         public string ID_Item {get; set;}
         public uint ID_Brand {get; set;}
         public float Size {get; set;} //restrict values from 14 to 49.5 with 0.5 step
@@ -9,8 +11,7 @@ namespace PZ.Models
         public string Style {get; set;} //https://en.wikipedia.org/wiki/List_of_shoe_styles
         public uint Stock {get; set;}
         public string ItemName {get; set;}
-        public enum Sex {mens=1, womens=2, unisex=3, boy=4, girl=5, unisexChild=6}
-
+        public SexType Sex { get; set; }
         //FK
         public Brands Brands {get; set;}
         
