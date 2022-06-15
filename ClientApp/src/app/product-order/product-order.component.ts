5575d3ff4edf540b46500f3942f18ef45917c503
pawszmir <pawszmir@gmail.com>
2022-06-15 18:35:19 +0200

    product order form init

---
+++ ClientApp/src/app/product-order/product-order.component.ts
@@ -0,0 +1,12 @@
+import { Component, Inject, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-order',
+  templateUrl: './product-order.component.html',
+  styleUrls: ['./product-order.component.css']
+
+})
+export class ProductOrderComponent {
+
+    constructor() {}
+}
