f3bb2bd1dc7c22d30e2966c6726aa85a53ce93a6
Michal Trefonski <trefonski@gmail.com>
2022-06-16 15:22:24 +0200

    ItemsModel_PriceFields

---
+++ Migrations/20220616132118_ItemsModel_PriceFields.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace V1.Migrations
+{
+    public partial class ItemsModel_PriceFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Discount",
+                table: "Items",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "NettPrice",
+                table: "Items",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "VAT",
+                table: "Items",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "NettPrice",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "VAT",
+                table: "Items");
+        }
+    }
+}
