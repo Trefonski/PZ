97eaecd77ff614144d611c25a63c8726549d86db
pawszmir <pawszmir@gmail.com>
2022-06-19 17:17:55 +0200

    geting data form database add to cart button

--- ClientApp/src/app/item-details/item-details.component.ts
+++ ClientApp/src/app/item-details/item-details.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import * as _ from 'lodash';
+import { EnvironmentUrlService } from '../shared/services/environment-url.service';
 
 
 @Component({
@@ -10,8 +12,31 @@ import * as _ from 'lodash';
 })
 export class ItemDetailsComponent {
   httpClient: any;
-  items: any;
+   item: Items = {} as Items;
   
-  constructor(private activateRoute: ActivatedRoute){}
+  constructor(private http: HttpClient, private envUrl: EnvironmentUrlService, private activateRoute: ActivatedRoute) {
+  let param1 = this.activateRoute.snapshot.params.id;
+;
+    http.get<Items>(`${this.envUrl.urlAddress}/Items/GetSingleItemJson`,{params: {ID_Item:param1}}).subscribe(result => {
+      this.item = result;
+    })
+  }
+  addToCart(){
+    let param1 = this.activateRoute.snapshot.params.id;
+    localStorage.setItem(param1, JSON.stringify(this.item));
+  }
+}
 
+interface Items {
+  iD_Item: string;
+  iD_Brand: number;
+  size: number;
+  colour: string;
+  style: string;
+  stock: number;
+  itemName: string;
+  sex: number;
+  discout: number;
+  nettPrice: number;
+  vat: number;
 }
--- ClientApp/src/app/items-view/items-view.component.ts
+++ ClientApp/src/app/items-view/items-view.component.ts
@@ -1,22 +1,40 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgControl } from '@angular/forms';
+import { EnvironmentUrlService } from '../shared/services/environment-url.service';
 
 @Component({
   selector: 'app-items-view',
   templateUrl: './items-view.component.html',
   styleUrls: ['./items-view.component.css']
 })
-export class ItemsViewComponent implements OnInit{
- items: any = [];
+export class ItemsViewComponent{
+  httpClient: any;
+  items: Items[] = [];
+  
 
- constructor(private httpClient: HttpClient){}
-   
-   ngOnInit() {
-     this.httpClient.get("https://localhost:44432/assets/item.json").subscribe(data =>{
-       this.items = data;
-       console.log(this.items.ID_Item);
-     })
-   }
-  }
+ constructor(private http: HttpClient, private envUrl: EnvironmentUrlService) {
+  
+   http.get<Items[]>(`${this.envUrl.urlAddress}/Items/GetItemsAllJson`).subscribe(result => {
+    this.items = result;
+   })
+ }
+ addToCart(){
+  localStorage.setItem('item', JSON.stringify(this.items as {}));
 
+}
+}
+
+interface Items {
+  iD_Item: string;
+  iD_Brand: number;
+  size: number;
+  colour: string;
+  style: string;
+  stock: number;
+  itemName: string;
+  sex: number;
+  discout: number;
+  nettPrice: number;
+  vat: number;
+}
