b597c72865ef2e64d898add86deca0f67eb206d9
pawszmir <pawszmir@gmail.com>
2022-06-19 16:56:18 +0200

    single style change and css file link added

--- ClientApp/src/app/admin/dashboard/admin-dashboard.component.ts
+++ ClientApp/src/app/admin/dashboard/admin-dashboard.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent {
 
