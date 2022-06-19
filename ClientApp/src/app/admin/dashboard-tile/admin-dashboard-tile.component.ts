37f1cf12ca493f24a2781a5717d2a7d6b45223ca
pawszmir <pawszmir@gmail.com>
2022-06-19 16:54:18 +0200

    single syle change and css link added

--- ClientApp/src/app/admin/dashboard-tile/admin-dashboard-tile.component.ts
+++ ClientApp/src/app/admin/dashboard-tile/admin-dashboard-tile.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'div[admin-dashboard-tile]',
   templateUrl: './admin-dashboard-tile.component.html',
+  styleUrls: ['./admin-dashboard-tile.component.css'],
   host: { 'class': 'col-12 col-sm-6 col-md-4 col-lg-3' }
 })
 export class AdminDashboardTileComponent {
